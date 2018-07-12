f76fcfff68e846c55781b99508a74b87fc5497f8
Haley Alexander <15005204+hal00alex@users.noreply.github.com>
2018-07-12 08:13:00 -0500

    Update Hex_Address_Calculator.py

--- Hex_Address_Calculator.py
+++ Hex_Address_Calculator.py
@@ -1,1 +1,16 @@
-
+def main():
+  print "Hello Master\n"
+  address = 0x0
+  next_address = 0x0
+  add_arr = []
+  #change for your needs
+  y = 20
+  inc = 0x1
+  for i in range (0, y):
+    #may need to change 0x1 to a different number
+    next_address = next_address + inc
+    add_arr.append(hex(next_address))
+  print (hex(next_address))
+  #can print array, or one item out of array, or send array to file
+  print "\nGoodbye Master\n"
+main()
